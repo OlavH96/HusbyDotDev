057876c16c45c0e8540938efd43ec5ce5f85e679
Olav Reppe Husby <olavh96@gmail.com>
2022-11-14 21:53:20 +0100

    Try add settings window

---
+++ src/P5/Settings.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState } from 'react';
+import './Settings.css';
+
+export type Options = {
+	planetSize: number;
+	planetMass: number;
+	planetColor: string;
+}
+
+type Props = {
+	settingsChanged: (options: Options) => void,
+	options: Options
+};
+
+export default function Settings(props: Props) {
+
+	return (
+		<div className='settings'>
+			<div>Settings</div>
+
+			<label>Planet size</label>
+			<input type="number" value={props.options.planetSize} onChange={e => { props.settingsChanged({ ...props.options, planetSize: Number.parseInt(e.target.value) }) }} />
+			<label>Planet color</label>
+			<input type="color" value={props.options.planetColor} onChange={e => { props.settingsChanged({ ...props.options, planetColor: e.target.value }) }} />
+		</div>
+	)
+}
