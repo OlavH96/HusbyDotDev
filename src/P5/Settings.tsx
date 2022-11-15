486234b6e5b7fd23a714a91db4ea713559e30954
Olav Reppe Husby <olavh96@gmail.com>
2022-11-15 20:50:56 +0100

    Add settings css, mass, reset buttons

--- src/P5/Settings.tsx
+++ src/P5/Settings.tsx
@@ -17,15 +17,24 @@ export default function Settings(props: Props) {
 
 	return (
 		<div className='settings'>
-			<div>Settings</div>
+			<h1>Settings</h1>
 
-			<label>Planet size</label>
-			<input type="number" value={props.options.planetSize} onChange={e => { props.settingsChanged({ ...props.options, planetSize: Number.parseInt(e.target.value) }) }} />
-			<label>Planet color</label>
-			<div>
+			<section>
+				<label>Planet size</label>
+				<input type="number" value={props.options.planetSize} onChange={e => { props.settingsChanged({ ...props.options, planetSize: Number.parseInt(e.target.value) }) }} />
+				<button onClick={e => { props.settingsChanged({ ...props.options, planetSize: 10 }) }}>Reset</button>
+			</section>
+			<section>
+				<label>Planet mass</label>
+				<input type="range" min="0.1" max="10" value={props.options.planetMass} onChange={e => { props.settingsChanged({ ...props.options, planetMass: Number.parseInt(e.target.value) }) }} />
+				<p>{props.options.planetMass}</p>
+				<button onClick={e => { props.settingsChanged({ ...props.options, planetMass: 1 }) }}>Reset</button>
+			</section>
+			<section>
+				<label>Planet color</label>
 				<input type="color" value={props.options.planetColor} onChange={e => { props.settingsChanged({ ...props.options, planetColor: e.target.value }) }} />
 				<button onClick={e => { props.settingsChanged({ ...props.options, planetColor: "random" }) }}>Reset</button>
-			</div>
+			</section>
 		</div>
 	)
 }
